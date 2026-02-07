f390ceae163b970e1d79ab91b5033d3e19e79d86
Anna Giermakowska <an.giermakowska@gmail.com>
2026-02-07 16:51:47 +0100

    zmiany

--- I/js/index1.js
+++ I/js/index1.js
@@ -1,2 +1,28 @@
 // funkcje to metoda 
 
+function test(a, b) {
+    // instrukcja 1
+    // instrukcja 2
+    // instrukcja 3
+    return 5
+}
+
+console.log(test())
+
+function dodaj(a, b) {
+    return a + b
+}
+
+const sumaZmiennych = dodaj(2, 7)
+console.log(sumaZmiennych)
+
+function dzielenie(x, y) {
+    if (y === 0) {
+        console.log('nie można dzielić przez zero')
+        return 
+    }
+    return x / y;
+}
+
+let wynikDzielenia = dzielenie(10, 5);
+console.log(wynikDzielenia)
