29e607f14583b0e0a7e8b1c11c910ca220dc853d
Anna Giermakowska <an.giermakowska@gmail.com>
2026-02-08 17:33:08 +0100

    zmiany

--- I/js/index2.js
+++ I/js/index2.js
@@ -25,7 +25,48 @@ let i = 0;
 let listaZakupow = document.getElementById("produkty").getElementsByTagName('li');
 
 
-while(i<listaZakupow.length){
-    rezultat.innerHTML += listaZakupow[i].innerHTML+"<br>"
-    i++
+// while(i<listaZakupow.length){
+//     rezultat.innerHTML += listaZakupow[i].innerHTML+"<br>"
+//     i++
+// }
+
+// for (let index = 0; index < listaZakupow.length; index++) {
+//     rezultat.innerHTML += listaZakupow[index].innerHTML+"<br>"
+
+// }
+
+// for (let index = 0; index < listaZakupow.length; index++) {
+//     if(index %2 ==! 0){
+//         listaZakupow[index].innerHTML =  "parzysty: " + listaZakupow[index].innerHTML
+//     }
+//     else
+//         continue
+//     alert(index)
+// }
+// object = {
+//     imie: "Agata",
+//     nazwisko: "Kowalska",
+//     wiek: 43
+// }
+
+// for (const key in object) {
+//     alert(object[key])
+// }
+
+let argument = [3, 4, 7, 6, 9, 78]
+const addNumbers = () => {
+    let sum = 0
+    for (let index = 0; index < argument.length; index++) {
+        sum += argument[index]
+    }
+    return sum
 }
+
+let sum = 0;
+for (const key in argument) {
+    sum += argument[key];
+}
+
+let suma = addNumbers(argument);
+console.log(suma)
+console.log(sum)
