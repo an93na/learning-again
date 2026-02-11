65a114edb9e262df6c90b3a78df3a9f2713624ae
Anna Giermakowska <an.giermakowska@gmail.com>
2026-02-11 20:49:14 +0100

    changes

---
+++ I/js/index2.js
@@ -0,0 +1,8 @@
+function wypiszTekst (tekst) {
+    console.log(tekst + "!!!")
+}
+
+let wybor = document.getElementById("wybor")
+wybor.onclick = function (){
+    console.log('kliknięto na tekst')
+}
