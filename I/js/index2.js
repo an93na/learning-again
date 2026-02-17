b4cab3f2ce509751e5a745a2dc2875b414150502
Anna Giermakowska <an.giermakowska@gmail.com>
2026-02-17 20:20:31 +0100

    changes

--- I/js/index2.js
+++ I/js/index2.js
@@ -6,3 +6,10 @@ let wybor = document.getElementById("wybor")
 wybor.onclick = function (){
     console.log('kliknięto na tekst')
 }
+
+
+function zmienKolor () {
+    this.style.color = "blue"
+}
+let test = document.getElementById("test");
+test.onmouseover = zmienKolor;
