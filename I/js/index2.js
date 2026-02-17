00e4377f2fe74e4d887027fff8e6a44f4a8eed5d
Anna Giermakowska <an.giermakowska@gmail.com>
2026-02-17 21:01:46 +0100

    changes

--- I/js/index2.js
+++ I/js/index2.js
@@ -14,7 +14,18 @@ function zmienKolor() {
 function zmienKolor2() {
     this.className = ""
 }
+const powiekszRozmiar = () => {
+    test.className = 'powieksz'
+}
+const zmniejszRozmiar = () => {
+    test.className = 'zmniejsz'
+}
 let test = document.getElementById("test");
 test.onmouseover = zmienKolor;
 test.onmouseleave = zmienKolor2;
 
+let increase = document.getElementById("+");
+let decrease = document.getElementById("-")
+
+increase.onclick = powiekszRozmiar;
+decrease.onclick = zmniejszRozmiar;
